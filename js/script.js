1a8bba80cdbd7b99a7c46e7ba1cd28bd4291470f
LiliaIsABell <lilisa_alugo@yahoo.com>
2023-03-12 23:28:57 -0400

    Added Home Page Assets

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/******************************************************
+
+This is a website that I will be using as my portfolio.
+
+Here is were I will set up any fun effects.
+
+******************************************************/
+
+
+
+// Added preload fuction
+// Used this to load images or sounds
+function preload() {
+
+}
+
+// setup()
+//
+// Sets up the basic elements of the screen
+// You can play sound here and set up other functions
+function setup() {
+  
+}
