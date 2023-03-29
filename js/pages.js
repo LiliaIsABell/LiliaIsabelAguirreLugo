1a637e01707310e68d109daaa4595eb38b29292d
LiliaIsABell <lilisa_alugo@yahoo.com>
2023-03-29 16:29:11 -0400

    Added References

---
+++ js/pages.js
@@ -0,0 +1,71 @@
+"use strict";
+
+/******************************************************
+
+
+Here is were I will set up the slideshow and dropdown elements
+
+******************************************************/
+
+
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+}
+
+function bodyguard() {
+  var x = document.getElementById("dropdown1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+function fangirl() {
+  var x = document.getElementById("dropdown2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function idol() {
+  var x = document.getElementById("dropdown3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
--- js/script.js
+++ js/script.js
@@ -2,16 +2,13 @@
 
 /******************************************************
 
-This is a website that I will be using as my portfolio.
 
-Here is were I will set up any fun effects.
+Here is were I will set up the moving background elements.
 
 ******************************************************/
 
 
 let circle = [1,2,3,4,5];
-let slideIndex = 1;
-
 
 // Added preload fuction
 // Used this to load images or sounds
@@ -53,57 +50,5 @@ function draw() {
   circle[4].move();
 
 
-showSlides(slideIndex);
 
-
-}
-
-
-
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-
-}
-
-function bodyguard() {
-  var x = document.getElementById("dropdown1");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
-
-function fangirl() {
-  var x = document.getElementById("dropdown2");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
 }
